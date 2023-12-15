fb157d45b626f833d3ef54e3a109d1d9836505a3
omiras <oscar.miras@gmail.com>
2023-12-15 11:10:37 +0100

    cambio del tipo de información sobre la evolución del pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,86 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 function App() {
+  const fetchSinglePokemon = async (pokemonName) => {
+    const responseSinglePokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const dataSinglePokemon = await responseSinglePokemon.json();
+    return dataSinglePokemon;
+  };
+
+  const fetchPokemonSpecies = async (urlSpacies) => {
+    const response = await fetch(urlSpacies);
+    const data = await response.json();
+    return data;
+  };
 
   const [pokemons, setPokemons] = useState([]);
 
-
-
   useEffect(() => {
-
-
     const fetchPokemons = async () => {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
       const data = await response.json();
 
       const mappedPokemons = [];
 
       for (const p of data.results) {
+        const dataSinglePokemon = await fetchSinglePokemon(p.name);
+        console.log(
+          "🚀 ~ file: App.jsx:31 ~ fetchPokemons ~ dataSinglePokemon:",
+          dataSinglePokemon
+        );
 
-        const responseSinglePokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${p.name}`);
-        const dataSinglePokemon = await responseSinglePokemon.json();
-
-
-        const responseEvolution = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${dataSinglePokemon.id}`);
-        const dataEvolution = await responseEvolution.json();
+        const dataSpecies = await fetchPokemonSpecies(
+          dataSinglePokemon.species.url
+        );
 
         mappedPokemons.push({
           id: dataSinglePokemon.id,
           name: dataSinglePokemon.name,
           image: dataSinglePokemon.sprites.front_default,
-          types: dataSinglePokemon.types.map(t => t.type.name),
-          evolutions: dataEvolution.chain.evolves_to.map(e => e.species.name)
+          types: dataSinglePokemon.types.map((t) => t.type.name),
+          evolvesFrom:
+            dataSpecies.evolves_from_species &&
+            dataSpecies.evolves_from_species.name,
         });
       }
       return mappedPokemons;
     };
 
-
-
-    fetchPokemons().then(allPokemons => setPokemons(allPokemons));
-
-  }, [])
+    fetchPokemons().then((allPokemons) => setPokemons(allPokemons));
+  }, []);
 
   return (
     <>
-      <div>{pokemons.map(p => <article style={{
-        border: "1px solid black"
-      }} key={p.id}>
-        <ul>
-          <li>Id: {p.id}</li>
-          <li>Name: {p.name}</li>
-          <li><img src={p.image}></img></li>
-          <li>Types: {p.types.join(' ')}</li>
-          <li>Evolutions: {p.evolutions.join(' ')}</li>
-        </ul>
-      </article>)}</div>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          flexWrap: "wrap",
+        }}
+      >
+        {pokemons.map((p) => (
+          <article
+            style={{
+              border: "1px solid black",
+            }}
+            key={p.id}
+          >
+            <ul>
+              <li>Id: {p.id}</li>
+              <li>Name: {p.name}</li>
+              <li>
+                <img src={p.image}></img>
+              </li>
+              <li>Types: {p.types.join(" ")}</li>
+              <li>Evolves from: {p.evolvesFrom}</li>
+            </ul>
+          </article>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
